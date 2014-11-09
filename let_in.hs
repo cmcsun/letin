48c9a617d1e9946d74a9ca52183ec6864eb7afaf
Methos <ggmethos@autistici.org>
2014-11-09 13:52:52 +0100

    let_in.hs

---
+++ let_in.hs
@@ -0,0 +1,9 @@
+main = print let_in
+let_in =let in'let'in=let in let in" let" in
+         let in let
+    let'in let_ _in =
+      let_>>_in in
+     in'let'in++
+            let in_let'in=let
+                  in " let in let" in let'in
+  in_let'in in'let'in
